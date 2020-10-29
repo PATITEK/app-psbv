9076b00e5851e6d52ce91c2cea203ad60308b438
Dau Anh Tuan <dauanhtuansg@gmail.com>
2020-10-29 23:09:49 +0700

    init component reset, change password

--- psbv-customer/src/app/auth-manager/auth-manager-routing.module.ts
+++ psbv-customer/src/app/auth-manager/auth-manager-routing.module.ts
@@ -11,7 +11,19 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'reset-password',
+    loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+  },
+  {
+    path: 'new-password',
+    loadChildren: () => import('./new-password/new-password.module').then( m => m.NewPasswordPageModule)
+  },
+  {
+    path: 'password-changed',
+    loadChildren: () => import('./password-changed/password-changed.module').then( m => m.PasswordChangedPageModule)
   }
+
 ];
 
 @NgModule({
