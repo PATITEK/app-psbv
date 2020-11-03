20dfde4f0a4b92bdf709d09554fff0dac95e33bb
hcmutemimi <hoaimiqng@gmail.com>
2020-11-03 11:09:58 +0700

    new password complete

--- psbv-customer/src/app/app-routing.module.ts
+++ psbv-customer/src/app/app-routing.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
   // {
   //   path: '',
   //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   // },
+
+  
+  // {
+  //   path: '',
+    
+  //   loadChildren: () => import('./auth-manager/auth-manager.module').then( m => m.AuthManagerPageModule)
+  // },
   {
-    path: '',
-    loadChildren: () => import('./auth-manager/auth-manager.module').then( m => m.AuthManagerPageModule)
-  }
+    path: 'new-password',
+    loadChildren: () => import('./auth-manager/new-password/new-password.module').then( m => m.NewPasswordPageModule)
+  },
+
 ];
 @NgModule({
   imports: [
