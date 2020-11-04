409214c23da8628035c91f693a9d51af1f2a132c
dau anh tuan <dauanhtuan@secure.janeto.com>
2020-11-04 18:47:51 +0700

    the tabs is uncorrect

--- psbv-customer/src/app/app-routing.module.ts
+++ psbv-customer/src/app/app-routing.module.ts
@@ -2,18 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  // },
+  {
+    path: 'main',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
   {
     path: 'auth',
     loadChildren: () => import('./auth-manager/auth-manager.module').then( m => m.AuthManagerPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'pages',
@@ -23,10 +18,6 @@ const routes: Routes = [
     path: 'statusNoti',
     loadChildren: () => import('./@modular/page-noti/page-noti.module').then( m => m.PageNotiModule)
   },
-  
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: '**', redirectTo: 'auth' },
-
 ];
 @NgModule({
   imports: [
