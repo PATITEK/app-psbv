2cf25ee0ca9a300c6665b5eaed2914ed440f3365
hcmutemimi <hoaimiqng@gmail.com>
2020-11-04 15:53:47 +0700

    home product, resset password

--- psbv-customer/src/app/app-routing.module.ts
+++ psbv-customer/src/app/app-routing.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
     path: 'statusNoti',
     loadChildren: () => import('./@modular/page-noti/page-noti.module').then( m => m.PageNotiModule)
   },
+  
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: '**', redirectTo: 'auth' },
 
