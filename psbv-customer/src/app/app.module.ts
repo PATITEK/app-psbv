cd70e5e3a9cd9b81e3d2349327185fd232994f87
hcmutemimi <hoaimiqng@gmail.com>
2020-11-09 16:43:31 +0700

    edit UI hidden/show password

--- psbv-customer/src/app/app.module.ts
+++ psbv-customer/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import {HttpClientModule} from '@angular/common/http'
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -14,7 +16,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule,FormsModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule,FormsModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
--- psbv-customer/src/app/auth-manager/password-changed/password-changed.page.ts
+++ psbv-customer/src/app/auth-manager/password-changed/password-changed.page.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-changed.page.scss'],
 })
 export class PasswordChangedPage implements OnInit {
+  public type = 'password';
+  public type2 = 'password';
+  public type3 = 'password';
+
+  public showPassCurrent = false;
+  public showPassNew = false;
+  public showPassNewAgain = false;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  showPasswordCurent(){
+    this.showPassCurrent = !this.showPassCurrent;
+    if(this.showPassCurrent){
+      this.type = 'text';
+    }
+    else {
+      this.type ='password';
+    }
+  }
+  showPasswordNew(){
+    this.showPassNew = !this.showPassNew;
+    if(this.showPassNew){
+      this.type2 = 'text';
+    }
+    else {
+      this.type2 ='password';
+    }
+  }
+  showPasswordNewAgain(){
+    this.showPassNewAgain = !this.showPassNewAgain;
+    if(this.showPassNewAgain){
+      this.type3 = 'text';
+    }
+    else {
+      this.type3 ='password';
+    }
+  }
 }
