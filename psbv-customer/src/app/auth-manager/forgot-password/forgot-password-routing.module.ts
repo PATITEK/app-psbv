49f87f1c04e1bf3396c7ff25e8e2e46609e7da1e
huu phuoc <gamemakerrpg1325@gmail.com>
2020-11-11 22:29:16 +0700

    fix scroll bug

--- psbv-customer/src/app/auth-manager/forgot-password/forgot-password-routing.module.ts
+++ psbv-customer/src/app/auth-manager/forgot-password/forgot-password-routing.module.ts
@@ -1,28 +1,3 @@
-<<<<<<< HEAD:psbv-customer/src/app/pages/pages-routing.module.ts
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { PagesPage } from './pages.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: PagesPage
-  },  {
-    path: 'products',
-    loadChildren: () => import('./products/products.module').then( m => m.ProductsPageModule)
-  },
-
-
-
-];
-
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-})
-export class PagesPageRoutingModule {}
-=======
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -40,4 +15,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class ForgotPasswordPageRoutingModule {}
->>>>>>> b30cbe75c26c19ef7c5079540314be693234345c:psbv-customer/src/app/auth-manager/forgot-password/forgot-password-routing.module.ts
