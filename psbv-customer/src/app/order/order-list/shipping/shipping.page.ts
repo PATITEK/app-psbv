6ffecef79c22f8bafabead67c315b1e755f1bd5a
phuoctai12t <phuoctai12t@gmail.com>
2020-11-18 15:31:17 +0700

    shipping

--- psbv-customer/src/app/order/order-list/shipping/shipping.page.ts
+++ psbv-customer/src/app/order/order-list/shipping/shipping.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { IStatus } from '../order-status/order-status.page';
 
 @Component({
   selector: 'app-shipping',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipping.page.scss'],
 })
 export class ShippingPage implements OnInit {
+  text: string;
+  statuses: IStatus[] = [
+    {
+      name: 'Received request',
+      time: '08:23am',
+      date: '12/12/1212',
+      didPassed: true
+    },
+    {
+      name: 'Arrive pick up point',
+      time: '08:54am',
+      date: '12/12/1212',
+      didPassed: true
+    },
+    {
+      name: 'Pick up items',
+      time: '09.05am',
+      date: '12/12/1212',
+      didPassed: true
+    },
+    {
+      name: 'Done',
+      didPassed: false
+    },
+    {
+      name: 'Received request 1',
+      didPassed: false
+    },
+    {
+      name: 'Received request 2',
+      didPassed: false
+    }
+  ];
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router,
+    public sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
+    const tabs = document.querySelectorAll('ion-tab-bar');
+    Object.keys(tabs).map((key) => {
+      tabs[key].style.display = 'none';
+    });
+    this.text = `calc(${this.countPassedItem()}%)`;
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
+  countPassedItem(): number {
+    let num: number = this.statuses.filter(status => status.didPassed).length;
+    console.log(num);
+    return num * 1.667 * 10 - 16.67;
+  }
 }
