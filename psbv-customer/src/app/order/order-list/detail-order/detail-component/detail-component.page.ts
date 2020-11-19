3426fef8c6b3f503a2f885b7760de170eb472e20
phuoctai12t <phuoctai12t@gmail.com>
2020-11-19 14:43:56 +0700

    detail order

---
+++ psbv-customer/src/app/order/order-list/detail-order/detail-component/detail-component.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-component',
+  templateUrl: './detail-component.page.html',
+  styleUrls: ['./detail-component.page.scss'],
+})
+export class DetailComponentPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- psbv-customer/src/app/order/order-list/detail-order/detail-order.page.ts
+++ psbv-customer/src/app/order/order-list/detail-order/detail-order.page.ts
@@ -1,6 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+
+export enum STATUS {
+  CONFIRMED,
+  DONE,
+  CANCEL
+};
+
+export interface ISubItem {
+  name: string;
+  desc1: string;
+  desc2?: string;
+}
+
+export interface IItem {
+  name: string;
+  status: STATUS;
+  subItems: ISubItem[];
+}
+
+export interface IAccessory {
+  src: string;
+  name: string;
+}
+
+export interface IProduct {
+  src: string;
+  amount: number;
+  name: string;
+  accessories: IAccessory[];
+}
 
 @Component({
   selector: 'app-detail-order',
@@ -9,9 +40,67 @@ import { Router } from '@angular/router';
 })
 export class DetailOrderPage implements OnInit {
 
+  text: string;
+  item: IItem = {
+    name: 'Item 12/12/1212',
+    status: STATUS.DONE,
+    subItems: [
+      {
+        name: 'ID',
+        desc1: '#1212121212'
+      },
+      {
+        name: 'Time shipping',
+        desc1: '12-12-1212',
+        desc2: '12:12'
+      },
+      {
+        name: 'Time received',
+        desc1: '13-12-1212',
+        desc2: '12:12'
+      }
+    ]
+  }
+
+  products: IProduct[] = [
+    {
+      src: 'https://dummyimage.com/165x165.jpg',
+      amount: 2,
+      name: 'Product 1',
+      accessories: [
+        {
+          src: 'https://dummyimage.com/90x90.jpg',
+          name: 'Accessory 1-1'
+        },
+        {
+          src: 'https://dummyimage.com/90x90.jpg',
+          name: 'Accessory 1-2'
+        },
+      ]
+    },
+    {
+      src: 'https://dummyimage.com/165x165.jpg',
+      amount: 99,
+      name: 'Product 2',
+      accessories: [
+        {
+          src: 'https://dummyimage.com/90x90.jpg',
+          name: 'Accessory 2-1'
+        }
+      ]
+    },
+    {
+      src: 'https://dummyimage.com/165x165.jpg',
+      amount: 3,
+      name: 'Product 3',
+      accessories: []
+    },
+  ]
+
   constructor(
     private location: Location,
-    private router: Router
+    private router: Router,
+    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit() {
@@ -19,10 +108,45 @@ export class DetailOrderPage implements OnInit {
     Object.keys(tabs).map((key) => {
       tabs[key].style.display = 'none';
     });
+    this.text = '20px 0';
   }
 
   goBack(): void {
     this.location.back();
   }
 
+  goToDetailComponent(): void {
+    this.router.navigateByUrl('/main/order/detail-order/detail-component')
+  }
+
+  getStatus(): any {
+    if (this.item.status === STATUS.CONFIRMED) {
+      return {
+        name: 'Confirmed',
+        background: '#b2e9fb'
+      }
+    }
+    if (this.item.status === STATUS.DONE) {
+      return {
+        name: 'Done',
+        background: '#91e29b'
+      }
+    }
+    return {
+      name: 'Cancel',
+      background: '#ce091c'
+    }
+  }
+
+  checkConfirmedStatus(): boolean {
+    return this.item.status === STATUS.CONFIRMED;
+  }
+
+  checkDoneStatus(): boolean {
+    return this.item.status === STATUS.DONE;
+  }
+
+  checkCancelStatus(): boolean {
+    return this.item.status === STATUS.CANCEL;
+  }
 }
