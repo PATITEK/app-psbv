571f4f30a657c984e69ca77ace8b2c8a966ac954
phuoctai12t <phuoctai12t@gmail.com>
2020-11-19 15:49:16 +0700

    detail component

--- psbv-customer/src/app/order/order-list/detail-order/detail-component/detail-component.page.ts
+++ psbv-customer/src/app/order/order-list/detail-order/detail-component/detail-component.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail-component',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponentPage implements OnInit {
 
-  constructor() { }
+  notes: string[] = [
+    'component note 1',
+    'component note 2 component note 2 component note 2 component note 2 component note 2',
+    'component note 3',
+    'component note 4'
+  ]
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    const tabs = document.querySelectorAll('ion-tab-bar');
+    Object.keys(tabs).map((key) => {
+      tabs[key].style.display = 'none';
+    });
   }
 
+  goBack(): void {
+    this.router.navigateByUrl('/main/order/detail-order');
+  }
 }
