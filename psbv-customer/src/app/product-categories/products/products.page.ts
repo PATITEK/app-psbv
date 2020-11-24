db8e930ea9730cc362a07ed9f7dfcde681808ec9
phuoctai12t <phuoctai12t@gmail.com>
2020-11-24 14:32:02 +0700

    copy

--- psbv-customer/src/app/product-categories/products/products.page.ts
+++ psbv-customer/src/app/product-categories/products/products.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { IPageRequest, ProductGroupsService } from 'src/app/@app-core/http';
 
 @Component({
   selector: 'app-products',
@@ -8,19 +9,72 @@ import { Location } from '@angular/common';
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
-  constructor(private location: Location) { }
+  @ViewChild(IonInfiniteScroll) infinityScroll: IonInfiniteScroll;
+
+  pageRequest: IPageRequest = {
+    page: 1,
+    per_page: 6,
+    total_objects: 20
+  }
+  data = [];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private productGroupService: ProductGroupsService
+  ) { }
+
   ngOnInit() {
     const tabs = document.querySelectorAll('ion-tab-bar');
-     Object.keys(tabs).map((key) => {
-    tabs[key].style.display = 'none';
+    Object.keys(tabs).map((key) => {
+      tabs[key].style.display = 'none';
     });
+    this.loadData();
   }
-  goToHome(): void {
-    this.location.back();
+
+  loadData() {
+    setTimeout(() => {
+      this.route.queryParams.subscribe((params) => {
+        this.productGroupService.getProductGroupDetail(JSON.parse(params['data']))
+        .subscribe(data => {
+          for (let item of data.products) {
+            this.data.push(item);
+          }
+          console.log(this.data);
+  
+          this.infinityScroll.complete();
+          this.pageRequest.page++;
+  
+          // check max data
+          if (this.data.length >= data.meta.pagination.total_objects) {
+            this.infinityScroll.disabled = true;
+          }
+        });
+      });
+    }, 500);
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('main/product-categories');
     const tabs = document.querySelectorAll('ion-tab-bar');
     Object.keys(tabs).map((key) => {
       tabs[key].style.display = 'flex';
     });
   }
 
+  goToNoti() {
+
+  }
+
+  goToUserInfo() {
+
+  }
+
+  goToDetail(item) {
+    this.router.navigate(['/main/home/product-info'], {
+      queryParams: {
+        data: JSON.stringify(item.id)
+      }
+    });
+  }
 }
