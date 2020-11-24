fca1cd95fc9391465ec89b1f6b40327800d24ec7
hcmutemimi <hoaimiqng@gmail.com>
2020-11-24 11:32:44 +0700

    merge from dev/tai

--- psbv-customer/src/app/auth-manager/sign-up/sign-up.module.ts
+++ psbv-customer/src/app/auth-manager/sign-up/sign-up.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
-
 import { SignUpPage } from './sign-up.page';
-
-
 @NgModule({
   imports: [
     CommonModule,
