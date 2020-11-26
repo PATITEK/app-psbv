3e9f386d099028b83742e2e707d9ef4ba1bc3fb2
phuoctai12t <phuoctai12t@gmail.com>
2020-11-26 08:08:28 +0700

    loading spinner

--- psbv-customer/src/app/home/home.page.ts
+++ psbv-customer/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonInfiniteScroll } from '@ionic/angular';
+import { IonInfiniteScroll, LoadingController } from '@ionic/angular';
 import { IPageRequest, ProductsService } from '../@app-core/http';
 import { PERMISSION } from './product-info/product-info.page';
 
@@ -14,15 +14,18 @@ export class HomePage implements OnInit {
 
   pageRequest: IPageRequest = {
     page: 1,
-    per_page: 6,
+    per_page: 10,
     total_objects: 20
   }
   data = [];
   permission: PERMISSION = PERMISSION.STANDARD;
+  isLoading = false;
+
 
   constructor(
     private router: Router,
-    private productService: ProductsService
+    private productService: ProductsService,
+    private loadingController: LoadingController
   ) { }
 
   goToDetail(item) {
@@ -35,6 +38,7 @@ export class HomePage implements OnInit {
   }
 
   ngOnInit() {
+    this.present();
     this.loadData();
   }
 
@@ -45,6 +49,7 @@ export class HomePage implements OnInit {
           this.data.push(item);
         }
         this.infinityScroll.complete();
+        this.dismiss();
         this.pageRequest.page++;
 
         // check max data
@@ -58,7 +63,7 @@ export class HomePage implements OnInit {
           this.pageRequest.per_page = temp;
         }
       })
-    }, 500);
+    }, 50);
   }
 
   checkGuestPermission(): boolean {
@@ -67,5 +72,22 @@ export class HomePage implements OnInit {
 
   checkStandardPermission(): boolean {
     return this.permission === PERMISSION.STANDARD;
+  }
+
+  async present() {
+    this.isLoading = true;
+    return await this.loadingController.create().then(a => {
+      a.present().then(() => {
+        console.log('presented');
+        if (!this.isLoading) {
+          a.dismiss().then(() => console.log('abort presenting'));
+        }
+      });
+    });
+  }
+
+  async dismiss() {
+    this.isLoading = false;
+    return await this.loadingController.dismiss().then(() => console.log('dismissed'));
   }
 }
